50f7e458a69bd5c91bdd0e87f88c60c1b881c209
Vlastimil Sadilek <vlastoun@gmail.com>
2016-11-11 08:14:37 +0100

    funkcni proogram

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-void PrintVector(vector<int>& array) {
+void PrintVector(vector<int> array) {
 	int size = array.size();
 	for (int i = 0; i < size; i++) {
 		cout << array[i] << " ";
@@ -24,25 +24,57 @@ void PrintVector(vector<int>& array) {
 
 int main () {
 	int MaxSize;
-	vector<BubbleClass> Permutations;
 	
 	cout << "Zadejte maximalni velikost pole:" << endl;
 	cin >> MaxSize;
-	for (int i = 0; i < MaxSize; i++) {
-		Permutations.push_back(BubbleClass(i+1));
-	}
-	cout << "Pole urcena k permutacim" << endl;
-	for (int i = 0; i < MaxSize; i++) {
-		Permutations[i].PrintInitArray();
-	}
-	vector < vector<int>> ArrayOfComparsions;
 	
-	for (int i = 0; i < MaxSize; i++) {
-		ArrayOfComparsions[i].push_back() = Permutations[i].GetArrayOfComparsions;
-	}
+	BubbleClass order1(MaxSize);
+	
+	
+	cout << "Vstupni pole:" << endl;
+	order1.PrintInitArray();
+	cout << endl;
 
+	cout << "Pole vymen:" << endl;
+	order1.PrintArrOfChanges();
+	cout << endl;
+	
+	cout << "Pole porovnani:" << endl;
+	order1.PrintArrOfComparsions();
+	cout << endl;
+	
+	vector<int> arrayOfChanges;
+	arrayOfChanges = order1.GetArrayOfChanges();
+	vector<int> arrayOfComparsions;
+	arrayOfComparsions = order1.GetArrayOfComparsions();
+	HistogramClass histogram1(arrayOfChanges);
+	
+	cout << "Pole cetnosti vymen:" << endl;
+	PrintVector(histogram1.GetHistogram());
+	cout << endl;
 
+	HistogramClass histogram2(arrayOfComparsions);
 
+	cout << "Pole cetnosti porovnani:" << endl;
+	PrintVector(histogram2.GetHistogram());
+	cout << endl;
+
+	vector<int> vectorToPrint;
+	vectorToPrint = histogram1.GetHistogram();
+	vector<int> vectorToPrint2;
+	vectorToPrint2 = histogram2.GetHistogram();
+
+	PrintToFileClass file1(vectorToPrint);
+	file1.SetNameOfFile("histogram cetnosti vymen");
+	file1.SetNameOfValues("pocet vymen");
+	file1.MakeFile();
+
+	PrintToFileClass file2(vectorToPrint2);
+	file1.SetNameOfFile("histogram cetnosti porovnani");
+	file1.SetNameOfValues("pocet porovnani");
+	file2.MakeFile();
+
+	cin >> MaxSize;
 	return 0;
 }
 
